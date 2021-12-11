edb13932ef3b487509b9870b08574df815c55c22
tilyes <tamas.ilyes23@gmail.com>
2021-12-11 08:26:06 +0100

    puzzle 11

---
+++ puzzle11/part_one.py
@@ -0,0 +1,45 @@
+FILENAME = './puzzle11/data/input'
+
+grid = []
+with open(FILENAME) as file:
+    for line in file:
+        grid.append([int(x) for x in list(line.strip())])
+
+n = 10
+
+def grid_step(grid):
+
+    flash_grid = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] += 1
+    s = 1
+    while s > 0:
+        s = 0
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] > 9 and flash_grid[i][j] == 0:
+                    s += 1
+                    flash_grid[i][j] = 1
+        for i in range(n):
+            for j in range(n):
+                if flash_grid[i][j] == 1:
+                    for i_f in range(i - 1, i + 2):
+                        for j_f in range(j - 1, j + 2):
+                            if (i_f, j_f) != (i, j) and i_f in range(n) and j_f in range(n):
+                                grid[i_f][j_f] += 1
+                    flash_grid[i][j] = 2
+
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] > 9:
+                grid[i][j] = 0
+            
+    return grid, flash_grid
+
+total = 0
+for step in range(100):
+    grid, flash_grid = grid_step(grid)
+    total += int(sum([sum(x) for x in flash_grid]) / 2)
+print(total)
